6d401f65d663f22c2bb8149a5f1947c00d3c279f
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-24 00:34:01 +0600

    navbar active hover and many other things

--- library/urls.py
+++ library/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.books, name="books"),
     path("book/<str:pk>/", views.book, name="book"),
-    path("book/create-book", views.createBook, name="create-book"),
+    path("create-book/", views.createBook, name="create-book"),
     path("update-book/<str:pk>/", views.updateBook, name="update-book"),
     path("delete-book/<str:pk>/", views.deleteBook, name="delete-book"),
     path("update-notice/<str:pk>/", views.noticeUpdate, name="notice-update"),
