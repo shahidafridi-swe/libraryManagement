b6346de22c04258f69eb4cd7e65289c6069e0021
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-05-06 13:56:11 +0600

    update enmail app

--- libraryManagement/settings.py
+++ libraryManagement/settings.py
@@ -68,22 +68,22 @@ WSGI_APPLICATION = 'libraryManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'library',
-        'HOST': 'localhost',
-        'USER':'root',
-        'PASSWORD':'7862'
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'library',
+#         'HOST': 'localhost',
+#         'USER':'root',
+#         'PASSWORD':'7862'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -123,7 +123,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
 EMAIL_HOST_USER = 'shahidafridi.pu@gmail.com'
-EMAIL_HOST_PASSWORD = 'tulqmbryohobiygr'
+EMAIL_HOST_PASSWORD = 'eqtaejlazeuibzze'
 
 
 # Static files (CSS, JavaScript, Images)
